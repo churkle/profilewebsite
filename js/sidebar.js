f5e33a5ab6a90b7d77ebbd95331b1bdf35464977
Charles Fei <charles.fei@vanderbilt.edu>
2015-12-23 17:12:47 -0600

    Added animations/buttons to show/hide menu bar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-	//$( ".navbar" ).hide();
+	$( ".navbar" ).hide();
+	$( ".hide-menu-button" ).hide();
 });
+
+function showSidebar(show){
+	if(show)
+	{
+		$( ".navbar" ).fadeIn(500);
+		$( ".menu-button" ).fadeOut(500);
+		$( ".hide-menu-button" ).show();
+	}
+	else
+	{
+		$( ".navbar" ).fadeOut(500);
+		$( ".menu-button" ).fadeIn(500);
+		$( ".hide-menu-button" ).fadeOut(500);
+	}
+
+}
